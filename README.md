5b856c52ee42570c9e9b6f741805202075ac542b
Zack Steinkamp <zack@steinkamp.us>
2025-01-24 21:46:22 -0800

    good start

---
+++ README.md
@@ -0,0 +1,27 @@
+# LineSeq
+
+LineSeq is a sequencer that you program by drawing a line or curve.
+
+You specify the time duration the line represents, as well as the lowest and highest notes. Send it a MIDI note, and LineSeq will transform or output a note according to where the line is at that time.
+
+![How it Looks](images/device.gif)
+
+## Installation
+
+[Download the newest .amxd file from the latest release](https://github.com/zsteinkamp/m4l-LineSeq/releaes) or clone this repository, and drag the `LineSeq.amxd` device into a track in Ableton Live.
+
+## Changelog
+
+* 2025-01-12 [v1](https://github.com/zsteinkamp/m4l-LineSeq/) - Initial release.
+
+## Usage
+...
+
+
+## TODO
+
+* ...
+
+## Contributing
+
+I'd love it if others extended this device. If you would like to contribute, simply fork this repo, make your changes, and open a pull request and I'll have a look.
---
+++ scaleAware.js
@@ -0,0 +1,87 @@
+inlets = 1;
+outlets = 1;
+autowatch = true;
+
+var state = {
+  scaleAware: 1,
+}
+
+var scaleMeta = {
+  notes: [],
+  watchers: {
+    root: null,
+    int: null,
+    mode: null,
+  }
+};
+
+
+function updateScales() {
+  //post('UPDATESCALES\n')
+  if (!scaleMeta.watchers.root) {
+    //post('NOROOTWATERH\n')
+    return;
+  }
+  if (!state.scaleAware) {
+    //post('NOTSCALEAWARE\n')
+    return
+  }
+
+  var api = new LiveAPI('live_set');
+  var root = api.get('root_note');
+  var intervals = api.get('scale_intervals');
+  scaleMeta.notes = [];
+
+  var root_note = root - 12;
+  var note = root_note;
+
+  // fill scaleMeta.notes with valid note numbers
+  while (note <= 127) {
+    for (var i = 0; i < intervals.length; i++) {
+      var interval = intervals[i];
+      note = root_note + interval;
+      if (note >= 0 && note <= 127) {
+        scaleMeta.notes.push(note);
+      }
+    }
+    root_note += 12;
+    note = root_note;
+  }
+  //post('SCALE ' + JSON.stringify(scaleMeta.notes) + '\n')
+}
+
+function quantizeNote(tag, noteNum) {
+  if (!state.scaleAware) {
+    outlet(0, tag, noteNum);
+    return
+  }
+  var i = 12;
+  for (var i = 0; i < 12; i++) {
+    var tryNote = noteNum - i;
+    if (scaleMeta.notes.indexOf(tryNote) > -1) {
+      //post('QUANTIZE: ' + noteNum + ' => ' + tryNote + '\n');
+      outlet(0, tag, tryNote);
+      return
+    }
+  }
+}
+
+function scaleAware(val) {
+  state.scaleAware = val;
+  updateScales();
+}
+
+function init() {
+  if (!scaleMeta.watchers.root) {
+    scaleMeta.watchers.root = new LiveAPI(updateScales, 'live_set')
+    scaleMeta.watchers.root.property = 'root_note'
+
+    scaleMeta.watchers.int = new LiveAPI(updateScales, 'live_set')
+    scaleMeta.watchers.int.property = 'scale_intervals'
+
+    scaleMeta.watchers.mode = new LiveAPI(updateScales, 'live_set')
+    scaleMeta.watchers.mode.property = 'scale_mode'
+  }
+}
+
+post('Reloaded scaleAware.js\n');
